9b985715a6884526d6f1b652ad181379edbdf168
Anthony Comeaux <acomeaux@athenahealth.com>
2023-08-21 11:12:12 -0500

    Adding in toggle cart functionality

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,9 +1,17 @@
 import './cart-icon.styles.scss';
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
+import { CartContext } from '../../contexts/cart.context';
+import { useContext } from 'react';
+
+
 
 const CartIcon = () => {
+    const {isCartOpen , setIsCartOpen} = useContext(CartContext);
+
+    const toggleDropdown = () => setIsCartOpen(!isCartOpen)
+
     return (
-        <div className='cart-icon-container'>
+        <div className='cart-icon-container' onClick={toggleDropdown}>
             <ShoppingIcon className='shopping-icon'/>
             <span className='item-count'>0</span>
         </div>
