2bbf7eb8975b5a86157ccd4d9c3316be2746bcc7
Anthony Comeaux <acomeaux@athenahealth.com>
2023-09-18 15:29:51 -0500

    Adding in style components library

--- src/components/category-preview/category-preview.component.jsx
+++ src/components/category-preview/category-preview.component.jsx
@@ -1,10 +1,16 @@
 import './category-preview.styles.scss';
 import ProductCard from '../product-card/product-card.component';
+import { Link } from 'react-router-dom';
 
 const CategoryPreview = ({title, products}) => {
     return (
+
         <div className='category-preview-container'>
-            <h2><span className='title'>{title.toUpperCase()}</span></h2>
+            <h2>
+                <Link to={`/shop/${title}`}>
+                    <span className='title'>{title.toUpperCase()}</span>
+                </Link>
+                </h2>
                 <div className='preview'>
                     {
                     products.filter((_, idx) => idx < 4).map((product) => {
--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,6 +1,6 @@
 import './category.styles.scss';
 import { useParams } from 'react-router-dom';
-import { useContext, useEffect, useState } from 'react';
+import { Fragment, useContext, useEffect, useState } from 'react';
 import { CategoriesContext } from '../../contexts/categories.context';
 import ProductCard from '../../components/product-card/product-card.component';
 
@@ -13,13 +13,18 @@ const Category = () => {
     }, [category, categoriesMap])
 
     return (
-        <div className='category-section-container'>
+        <Fragment>
+            <h2 className='category-title'>{category.toUpperCase()}</h2>
+            <div className='category-section-container'>
+            
             {products && 
                 products.map((product) => (
                     <ProductCard key={product.id} product={product} />
                 ))    
             }
         </div>
+        </Fragment>
+        
     )
 }
 
