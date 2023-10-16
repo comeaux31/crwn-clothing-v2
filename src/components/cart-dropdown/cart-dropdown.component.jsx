d6ce638d9f4511f00f471b207247c0b4077cfcbb
Anthony Comeaux <acomeaux@athenahealth.com>
2023-10-16 13:09:45 -0500

    More component conversion

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,9 +1,10 @@
-import './cart-dropdown.styles.scss';
+import './cart-dropdown.styles.jsx';
 import Button, {buttonTypesClasses} from '../button/button.component';
 import CartItem from '../cart-item/cart-item.component';
 import { CartContext } from '../../contexts/cart.context';
 import { useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { CartDropdownContainer, CartItems, EmptyMessage } from './cart-dropdown.styles.jsx';
 
 const CartDropdown = () => {
     const {cartItems, setIsCartOpen} = useContext(CartContext)
@@ -14,17 +15,17 @@ const CartDropdown = () => {
     }
 
     return (
-        <div className='cart-dropdown-container'>
-            <div className='cart-items'> 
+        <CartDropdownContainer>
+            <CartItems> 
             {cartItems.length ? 
             (cartItems.map(item => <CartItem key = {item.id} cartItem={item}/>)) : (
-                <span className='empty-message'>Your cart is empty</span>
+                <EmptyMessage>Your cart is empty</EmptyMessage>
               )}
-               </div>
-                    <Button onClick={goToCheckoutHandler}>Go to Checkout</Button>
+               </CartItems>
+                    <Button buttonType={buttonTypesClasses.base} onClick={goToCheckoutHandler}>Go to Checkout</Button>
                
             
-        </div>
+        </CartDropdownContainer>
     )
 }
 
